7ebc60316605fad9eae51834460da9b9a619943f
Zhizhou Lu <luzhizhou@hotmail.com>
2022-09-16 09:03:15 -0600

    bug fix

--- AXIA-wiki/sidebars.js
+++ AXIA-wiki/sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
       label: "Introduction",
       items: [
         "general/AXIA-Overview",
-        "general/AXIA-TestNet",
+        "general/AXIA-Testnet",
         //"general/getting-started",
         //"general/claims",
         //"general/redenomination",
